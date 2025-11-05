81ed6b873c037cb4614e06f2d1094b9d51f12cfb
Nguyen Quan <quannguyen5025@gmail.com>
2025-11-05 13:19:48 +0700

    mới làm phần danh muc. Món ăn đang làm tới giỏ hàng, vừa tạo model CratItem

---
+++ FAST FOOD/Models/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAST_FOOD.Models
+{
+    public class CartItem
+    {
+        public int MonAnId { get; set; }
+        public string TenMon { get; set; }
+        public decimal Gia { get; set; }
+        public string HinhAnh { get; set; }
+        public int SoLuong { get; set; }
+        public decimal ThanhTien => Gia * SoLuong;
+    }
+}
